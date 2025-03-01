31c71177600cd0f1cd161c612e80f9a4f6b3f118
anass Nammal <anassnammal@gmail.com>
2025-03-01 03:48:49 +0000

    adding navbar

--- task-manager/app/(core)/dashboard/page.tsx
+++ task-manager/app/(core)/dashboard/page.tsx
@@ -1,9 +1,67 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Task } from "@/lib/type";
+
+const getDataStatistics = (data: Task[] | null) => {
+  const statistics = {
+    total: data?.length ?? 0,
+    todo: 0,
+    inProgress: 0,
+    completed: 0,
+  }
+  data?.forEach(t => {
+    if (t.status === 'Todo') statistics.todo += 1;
+    else if (t.status === 'In Progress') statistics.inProgress += 1;
+    else if (t.status === 'Completed') statistics.completed += 1;
+  })
+  return statistics;
+}
 
 export default function Home() {
-    return (
-     <>
-      hello from root
-     </>
-    );
-  }
-  
+  const statistics = getDataStatistics(null)
+  return (
+    <div className="grid grid-cols-3 container mx-auto py-10 gap-2">
+      <div className="col-span-3 text-center text-3xl pb-4 font-bold">
+        <h1>Dashboard</h1>
+      </div>
+      <Card className="col-span-3">
+        <CardHeader>
+          <CardTitle>Total Tasks</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>{statistics.total}</p>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Todos</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>{statistics.todo}</p>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>In Progress</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>{statistics.inProgress}</p>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Completed</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>{statistics.completed}</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- task-manager/app/(core)/layout.tsx
+++ task-manager/app/(core)/layout.tsx
@@ -1,13 +1,40 @@
+"use client";
+
 import DialogProvider from "@/components/DialogProvider";
 import CacheProvider from "@/components/CacheProvider";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ListTodo, LayoutDashboard } from "lucide-react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const path = usePathname();
+
+  const nav = {
+    '/dashboard': {
+        title: 'Tasks',
+        icon: ListTodo,
+        href: '/todos'
+    },
+    '/todos': {
+        title: 'Dashboard',
+        icon: LayoutDashboard,
+        href: '/dashboard'
+    }
+  }
+  const currentNav = nav[path as '/dashboard' | '/todos'];
+
   return (
     <>
+      <div className="mx-4 mt-2">
+          <Link href={currentNav.href}  className="flex items-center gap-2">
+            <currentNav.icon />
+            <span className="text-xl underline">{currentNav.title}</span>
+          </Link>
+      </div>
       <CacheProvider>
         <DialogProvider>{children}</DialogProvider>
       </CacheProvider>
